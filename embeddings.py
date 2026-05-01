04c47c9dc58b39a8b28a68a36d9b151644453512
Kartik Patil <kartikppatil40@gmail.com>
2026-05-01 17:25:44 +0530

    fix: lazy-load BGE embedding model to reduce startup memory on Render free tier

--- embeddings.py
+++ embeddings.py
@@ -1,13 +1,18 @@
 from chromadb.api.types import EmbeddingFunction, Documents, Embeddings
-from sentence_transformers import SentenceTransformer
 
 
 class BGEEmbeddingFunction(EmbeddingFunction[Documents]):
     def __init__(self):
-        self.model = SentenceTransformer("BAAI/bge-small-en-v1.5")
+        self._model = None
+
+    def _load_model(self):
+        if self._model is None:
+            from sentence_transformers import SentenceTransformer
+            self._model = SentenceTransformer("BAAI/bge-small-en-v1.5")
 
     def __call__(self, input: Documents) -> Embeddings:
-        embeddings = self.model.encode(input, normalize_embeddings=True)
+        self._load_model()
+        embeddings = self._model.encode(input, normalize_embeddings=True)
         return embeddings.tolist()
 
 
